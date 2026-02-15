086aa7bbf81097bab0869e438663994799d10de2
Zebrar <777evgeniy2301@gmail.com>
2026-02-15 17:52:13 +0500

    feat: Добавил метод для показа страшных изображений.

--- Assets/Scripts/ButtonsLogic.cs
+++ Assets/Scripts/ButtonsLogic.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using System.Collections;
 
 public class ButtonsLogic : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class ButtonsLogic : MonoBehaviour
     
     private int CurrentImage = 0;
     private System.Random rnd = new System.Random();
+    public int TotalSkippedImages = 0;
+    private int ScaryChance = 0;
 
     void Start()
     {
@@ -32,12 +35,14 @@ public class ButtonsLogic : MonoBehaviour
     private void NextImage()
     {
         CurrentImage++;
+        TotalSkippedImages++;
         UpdateImage();
     }
 
     private void UpdateImage()
     {
         int chance = rnd.Next(2); 
+        int ShowScaryImageChance = rnd.Next(ScaryChance);
         Debug.Log("Шанс (1 = Normal): " + chance);
 
         if (chance == 1 && NormalImages.Length > 0)
@@ -45,6 +50,25 @@ public class ButtonsLogic : MonoBehaviour
             int index = CurrentImage % NormalImages.Length;
             MainImage.sprite = NormalImages[index];
             return; 
+        }
+
+        switch (TotalSkippedImages)
+        {
+            case 20:
+                ScaryChance = 10;
+                break;
+            case 30:
+                ScaryChance = 5;
+                break;
+            case 50:
+                ScaryChance = 2;
+                break;        
+        }
+        if (ShowScaryImageChance == 1 && ScaryImages.Length > 0)
+        {
+            int index = CurrentImage % ScaryImages.Length;
+            MainImage.sprite = ScaryImages[index];
+            return;
         }
 
         if (Memasiki.Length > 0)
