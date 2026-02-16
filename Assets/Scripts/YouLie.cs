9d398bdb97b351f61a0ee77af056f108addeaea5
Zebrar <777evgeniy2301@gmail.com>
2026-02-16 11:20:12 +0500

    fix: Исправил баг, при котором в цикле for не было задержки от куратины

--- Assets/Scripts/YouLie.cs
+++ Assets/Scripts/YouLie.cs
@@ -7,28 +7,37 @@ public class YouLie : MonoBehaviour
     public GameObject Text;
     public Transform canvasTransform; 
     private System.Random rnd = new System.Random();
+    public FakeCrash crash;
     int x = 0;
     int y = 0;
+    int i = 0;
     public void StartLie()
     {
-        YouLiee();
+        StartCoroutine(WaitAndDoLie(0.01f));
     }
     public void YouLiee()
     {
-        for(int i = 0; i < 200; i++)
+        i++;
+        if(i >= 100)
+        {
+            Debug.Log("СТОП!");
+        } else
         {
             int x = rnd.Next(-750, 750);
             int y = rnd.Next(-350, 350);
             GameObject newText = Instantiate(Text, canvasTransform);
             newText.transform.localPosition = new Vector3(x, y, 0);
-            StartCoroutine(WaitAndDoLie(0.05f)); 
-        }     
+            StartCoroutine(WaitAndDoLie(0.01f));
+        }
+                 
     }
 
     IEnumerator WaitAndDoLie(float delay)
     {
         // Пауза
         yield return new WaitForSeconds(delay);
+
+        YouLiee();
     }
 
 }
